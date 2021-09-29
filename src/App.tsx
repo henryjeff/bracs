bf841641b40ce749205cac4be177426cb778c0d8
henryjeff <henryheffernan@gmail.com>
2021-09-29 19:52:49 -0400

    app updates

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Bracket from "./components/bracket/Bracket";
+
+function App() {
+  return (
+    <div className="App" style={{ height: 4000, width: 4000 }}>
+      <Bracket />
+    </div>
+  );
+}
+
+export default App;
