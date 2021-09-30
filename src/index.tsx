49b18a55fc3c93f62317636b97a85614b2f58826
henryjeff <henryheffernan@gmail.com>
2021-09-29 20:02:00 -0400

    index fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+// import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Amplify from "aws-amplify";
+// import Amplify from "aws-amplify";
 // import config from "./aws-exports";
 // Amplify.configure(config);
 
