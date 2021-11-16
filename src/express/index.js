51a94468d4cdce61269d570bde0eb8087c40c0b5
henryjeff <henryheffernan@gmail.com>
2021-11-16 16:45:36 -0500

    more changes

--- src/express/index.js
+++ src/express/index.js
@@ -2,15 +2,18 @@ const path = require("path");
 const express = require("express");
 const app = express(); // create express app
 
+const PORT = process.env.HTTP_PORT || 8080;
+
 // add middle-wares
-app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static(path.join(__dirname, "..", "..", "build")));
 app.use(express.static("public"));
 
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+app.get("/*", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "..", "build", "index.html"));
 });
 
+
 // start express server on port 8080
-app.listen(8080, () => {
-  console.log("server started on port 8080");
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}.`);
 });
