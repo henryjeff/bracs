123b35c1992c01af5a6b069163d13d9d5dd0d2d8
henryjeff <henryheffernan@gmail.com>
2022-02-17 16:50:21 -0500

    high level app changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import { persistor, store } from "./store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
@@ -14,17 +19,36 @@ import NotFoundRoute from "./routes/NotFound";
 import ButtonPartyRoute from "./routes/Secret";
 
 import "./App.css";
+import { useTokenData } from "./store/selectors";
 
 // require('dotenv').config({path: `${__dirname}/.env`})
 // console.log(process.env)
+
+function PrivateRoute({ ...rest }: any) {
+  const tokenData = useTokenData();
+
+  if (!tokenData?.accessToken) {
+    return <Redirect to="/login" />;
+  }
+  return <Route {...rest} />;
+}
+
+function AnonymousRoute({ ...rest }: any) {
+  const tokenData = useTokenData();
+
+  if (tokenData?.accessToken) {
+    return <Redirect to="/" />;
+  }
+  return <Route {...rest} />;
+}
 
 function Routes() {
   return (
     <div style={styles.body}>
       <Switch>
         {/** Public Routes */}
-        <Route exact path="/login" component={SignInRoute} />
-        <Route exact path="/signup" component={SignUpRoute} />
+        <AnonymousRoute exact path="/login" component={SignInRoute} />
+        <AnonymousRoute exact path="/signup" component={SignUpRoute} />
         <Route exact path="/" component={HomeRoute} />
         <Route exact path="/about" component={AboutRoute} />
         <Route exact path="/view/:bracketId" component={BracketRoute} />
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-// import Amplify from "aws-amplify";
-// import config from "./aws-exports";
-// Amplify.configure(config);
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
