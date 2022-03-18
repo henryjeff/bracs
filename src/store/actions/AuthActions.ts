b65bedf14969eaa9407c16a6eea1da691c93b831
henryjeff <henryheffernan@gmail.com>
2022-03-18 17:05:39 -0400

    added create user auth action

--- src/store/actions/AuthActions.ts
+++ src/store/actions/AuthActions.ts
@@ -22,17 +22,22 @@ export const authLogin =
     });
   };
 
-// export const authCreateUser =
-//   (user: UserCreateRequestDto) =>
-//   (dispatch: (a: AuthCreateUserAction) => any, getState: () => RootState) => {
-//     return AuthController.create({ resource: user })
-//       .then((userRes) => {
-//         console.log("User created successfully");
-//       })
-//       .catch((err) => {
-//         return err;
-//       });
-//   };
+export const authCreateUser =
+  (user: UserCreateRequestDto) =>
+  (dispatch: (a: AuthCreateUserAction) => any, getState: () => RootState) => {
+    return new Promise((resolve, reject) => {
+      AuthController.create({ resource: user })
+        .then((res) => {
+          dispatch({
+            type: ActionType.CREATE_USER,
+          });
+          resolve(res);
+        })
+        .catch((e) => {
+          reject(e);
+        });
+    });
+  };
 
 export const authLogout =
   () => (dispatch: (a: AuthLogoutAction) => any, getState: () => RootState) => {
