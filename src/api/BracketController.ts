383eed80070e75a8bb4b8f9b499b18879873d918
henryjeff <henryheffernan@gmail.com>
2022-04-21 21:17:58 -0400

    added update bracket endpoint

--- src/api/BracketController.ts
+++ src/api/BracketController.ts
@@ -1,62 +1,87 @@
 import { getConfigs, axios, IRequestOptions, IRequestConfig } from "./";
 
 export class BracketController {
+  static postBracket(
+    params: {
+      bracketData: JSON;
+    } = {} as any,
+    options: IRequestOptions = {}
+  ): Promise<BracketCreateResponseDto> {
+    return new Promise((resolve, reject) => {
+      const url = "post_bracket";
+      const configs: IRequestConfig = getConfigs(
+        "post",
+        "application/json",
+        url,
+        options
+      );
+      const data = {
+        bracketData: params.bracketData,
+      };
+      configs.data = data;
+      axios(configs, resolve, reject);
+    });
+  }
 
-    static postBracket(
-        params: {
-        bracketData: JSON;
-        } = {} as any,
-        options: IRequestOptions = {}
-    ): Promise<BracketCreateResponseDto> {
-        return new Promise((resolve, reject) => {
-        const url = "post_bracket";
-        const configs: IRequestConfig = getConfigs(
-            "post",
-            "application/json",
-            url,
-            options
-        );
-        const data = {
-            bracketData: params.bracketData,
-        };
-        configs.data = data;
-        axios(configs, resolve, reject);
-        });
-    }
+  static updateBracket(
+    params: {
+      bracketID: number;
+      name: string;
+      bracketData: JSON;
+    } = {} as any,
+    options: IRequestOptions = {}
+  ): Promise<BracketUpdateResponseDto> {
+    return new Promise((resolve, reject) => {
+      const url = "modify_bracket";
+      const configs: IRequestConfig = getConfigs(
+        "put",
+        "application/json",
+        url,
+        options
+      );
+      const data = {
+        bracketID: params.bracketID,
+        name: params.name,
+        bracketData: params.bracketData,
+      };
+      configs.data = data;
+      axios(configs, resolve, reject);
+    });
+  }
 
-    static getUserBrackets(
-        params: {
-        userID: number;
-        } = {} as any,
-        options: IRequestOptions = {}
-    ): Promise<BracketGetListResponseDto> {
-        return new Promise((resolve, reject) => {
-        const url = `get_user_brackets/${params.userID}`;
-        const configs: IRequestConfig = getConfigs(
-            "get",
-            "application/json",
-            url,
-            options
-        );
-        axios(configs, resolve, reject);
-        });
-    }
+  static getUserBrackets(
+    params: {
+      userID: number;
+    } = {} as any,
+    options: IRequestOptions = {}
+  ): Promise<BracketGetListResponseDto> {
+    return new Promise((resolve, reject) => {
+      const url = `get_user_brackets/${params.userID}`;
+      const configs: IRequestConfig = getConfigs(
+        "get",
+        "application/json",
+        url,
+        options
+      );
+      axios(configs, resolve, reject);
+    });
+  }
 
-    static getBracket(
-        params: {
-        bracketID: number;
-        } = {} as any,
-        options: IRequestOptions = {}
-    ): Promise<BracketGetResponseDto> {
-        return new Promise((resolve, reject) => {
-        const url = `bracket/${params.bracketID}`;
-        const configs: IRequestConfig = getConfigs(
-            "get",
-            "application/json",
-            url,
-            options
-        );
-        axios(configs, resolve, reject);
-        });
-    }
+  static getBracket(
+    params: {
+      bracketID: number;
+    } = {} as any,
+    options: IRequestOptions = {}
+  ): Promise<BracketGetResponseDto> {
+    return new Promise((resolve, reject) => {
+      const url = `bracket/${params.bracketID}`;
+      const configs: IRequestConfig = getConfigs(
+        "get",
+        "application/json",
+        url,
+        options
+      );
+      axios(configs, resolve, reject);
+    });
+  }
 }
